61bc52a91ac91cf6bd8cc511280c0abd48a1b27f
PadmaDurairaj <70235145+PadmaDurairaj@users.noreply.github.com>
2020-08-25 22:32:35 -0400

    Create MinMaxSum.cs

---
+++ Min-Max Sum/MinMaxSum.cs
@@ -0,0 +1,30 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] array) {
+        long[] arr = Array.ConvertAll(array, val => (long)val);
+long maxCalc = arr.Sum()-arr.Min();
+long minCalc = arr.Sum()-arr.Max();
+Console.WriteLine("{0} {1}",minCalc,maxCalc);
+    }
+
+    static void Main(string[] args) {
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        miniMaxSum(arr);
+    }
+}
